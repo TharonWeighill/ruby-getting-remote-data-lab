e8694487e183931687bb0ed4cdadaa14b0921ab9
tharon <tharonweighill@gmail.com>
2020-12-03 00:19:58 -0700

    commit tree b33b1701a8f28266428ed0a50db26433de76b4ee

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,27 @@
 # Write your code here
+class GetRequester
+    require "net/http"
+   
+    URL = ""
+
+    def initialze(url)
+     url = URL
+    end    
+
+
+    def get_response_body
+        uri = URI.parse(URL)
+        response = NET::HTTP.get_response_body(uri)
+        response.body
+    end 
+
+
+    def program_school
+                programs = JSON.parse(self.get_programs)
+          programs.collect do |program|
+            program["agency"]  
+          end
+        end 
+
+
+end 
